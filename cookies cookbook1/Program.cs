b6bd77ebce94201380ec9fdbb1f4ce2d3ce5eaff
Tarirohope <priscilakapuyanyika974@gmail.com>
2026-03-09 10:21:51 +0200

    Add project files.

---
+++ cookies cookbook1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cookies_cookbook1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
